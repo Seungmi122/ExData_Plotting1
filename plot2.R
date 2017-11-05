0b4c6655e7ecbddb6a0c86a3453d57bb09f9cc0f
Oh,Seungmi <walrus8012@naver.com>
2017-11-05 23:25:36 +0900

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,38 @@
+data <- './Rproject/household_power_consumption.txt'
+household_power_consumption <- read.table(data,sep = ';', header = TRUE, stringsAsFactors = FALSE)
+household_power_consumption$Date <- as.Date(strptime(household_power_consumption$Date,'%d/%m/%Y'))
+attach(household_power_consumption)
+twodays <- household_power_consumption[Date == '2007-02-01'| Date == '2007-02-02' ,]
+detach(household_power_consumption)
+attach(twodays)
+Sys.setlocale("LC_TIME", "C") ## As I'm not in USA, I have to transform the system.
+
+twodays$Datetime <- paste(Date,Time)
+twodays$Datetime <- as.POSIXct(twodays$Datetime)
+attach(twodays)
+png('plot2.png')
+plot(Datetime,Global_active_power,type = 'l', ylab = 'Global Active Power (kilowatts)',xlab="")
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ plot3.R
@@ -0,0 +1,24 @@
+data <- './Rproject/household_power_consumption.txt'
+household_power_consumption <- read.table(data,sep = ';', header = TRUE, stringsAsFactors = FALSE)
+household_power_consumption$Date <- as.Date(strptime(household_power_consumption$Date,'%d/%m/%Y'))
+attach(household_power_consumption)
+twodays <- household_power_consumption[Date == '2007-02-01'| Date == '2007-02-02' ,]
+detach(household_power_consumption)
+attach(twodays)
+
+twodays$Global_active_power <- as.numeric(Global_active_power)
+Sys.setlocale("LC_TIME", "C") ## As I'm not in USA, I have to transform the system.
+
+twodays$Datetime <- paste(Date,Time)
+twodays$Datetime <- as.POSIXct(twodays$Datetime)
+attach(twodays)
+
+png('plot3.png')
+plot(Datetime,Sub_metering_1,type = 'l', ylab = 'Energy sub metering',xlab="")
+lines(Datetime,Sub_metering_2,col = "red")
+lines(Datetime,Sub_metering_3,col = "blue")
+legends <-c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+legend("topright",legend = legends, col = c("black","red","blue"),lty =1,cex = 0.8)
+
+
+dev.off()
